63174ea35dca92f75dbee0ef102d5fffa8483ab3
imrahul05 <rahulkumar20000516@gmail.com>
2025-07-10 18:00:58 +0530

    fi the cors error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors({
     origin: [
-        "https://crms-frontend-theta.vercel.app/",
+        "https://crms-frontend-theta.vercel.app",
         "http://localhost:5173"
     ],
     credentials: true,
